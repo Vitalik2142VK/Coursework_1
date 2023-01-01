0b5fdb79853629fe099d2006d2716caaa7392730
PC <ya.brodskii@yandex.ru>
2023-01-01 17:00:04 +0300

    Реализован класс Employee

---
+++ src/Employee.java
@@ -0,0 +1,49 @@
+public class Employee
+{
+    Employee(String name, int department, int wages) throws Exception {
+        id++;
+        idEmployee = id;
+        this.name = name;
+        if (department <= 0 || department >= 6) throw new Exception("Не корректно указан отдел сотрудника. Отделов модет быть от 1 до 5!");
+        this.department = department;
+        this.wages = wages;
+    }
+
+    private static int id = 0;
+    private int idEmployee; // индификатор сотрудника
+    private String name; // имя сотрудника
+    private int department; // отдел сотрудника
+    private int wages; // зарплата сотрудника
+
+    // Геттеры
+    public static int GetId()
+    {
+        return id;
+    }
+    public int GetIdEmployee()
+    {
+        return idEmployee;
+    }
+    public String GetName()
+    {
+        return name;
+    }
+    public int GetDepartment()
+    {
+        return department;
+    }
+    public int GetWages()
+    {
+        return wages;
+    }
+
+    // Сеттеры
+    public void SetDepartment(int department)
+    {
+        this.department = department;
+    }
+    public void SetSalary(int salary)
+    {
+        this.wages = salary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 public class Main
 {
+    public static void Get_list_employees(int size, Employee[] cs)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            System.out.println("Индекс сотрудника: " + cs[i].GetIdEmployee() + "\nИмя сотрудника: " + cs[i].GetName() + "\nОтдел: " + cs[i].GetDepartment() + "\nЗаработная плата: " + cs[i].GetWages() + "\n");
+        }
+    }
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        Employee[] companyStaff = new Employee[10];
+        try
+        {
+            companyStaff[0] = new Employee("Иван", 1, 50000);
+            companyStaff[1] = new Employee("Павел", 2, 7500);
+            companyStaff[2] = new Employee("Дарья", 3, 36000);
+            companyStaff[3] = new Employee("Ольга", 4, 73000);
+            companyStaff[4] = new Employee("Даннил", 5, 66000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        Get_list_employees(Employee.GetId(), companyStaff);
+
     }
 }
